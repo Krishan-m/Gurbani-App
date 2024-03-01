5613db0aa817f118b2d67a4f52cb78c3c6d75beb
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2024-02-29 23:35:02 -0500

    Add voice search feature

--- main.py
+++ main.py
@@ -1,26 +1,77 @@
 import banidb
+import speech_recognition as sr
 
-def getShabadMatches(verseInit):
-    matchingShabads = banidb.search(verseInit, searchtype=0)
+def getShabadQuery():
+    # return input("Find Shabad: ")
+    speechInput = search_from_speech()
+    return {'query': turn_into_bani_query(speechInput), 
+            'verse': speechInput}
+
+def getShabadMatches():
+    while True:
+        try:
+            shabadOrg = getShabadQuery()
+            shabadQuery = shabadOrg['query']
+            shabadVerse = shabadOrg['verse']
+            matchingShabads = banidb.search(shabadQuery, searchtype=0)
+            break
+        except Exception as e:
+            print(e)
+
     matchStore = {}
-    for page, matches in matchingShabads['pages_data'].items():
-        # print(page, matches)
+    print(shabadOrg)
+    for matches in matchingShabads['pages_data'].values():
         for shabad in matches:
-            print(f"{len(matchStore) + 1}. {shabad['verse']}")
-            matchStore[len(matchStore) + 1] = {'shabad_id': shabad['shabad_id'], 'verse': shabad['verse']}
+            print(f"{len(matchStore) + 1}. {shabad['verse']} ({shabad['source']['writer']})")
+            matchStore[len(matchStore) + 1] = shabad
 
     if (len(matchStore) == 0): print("No results found") 
     else: 
-        print(f"{len(matchStore)} results found.")
-        shabadChoice = eval(input("Enter shabad number: "))
-        shabad_id = matchStore[shabadChoice]['shabad_id']
-        # print(shabad_id)
-        displayShabad(shabad_id)
+        # print(f"{len(matchStore)} results found.")
+        # shabadChoice = eval(input("Enter shabad number: "))
+        # shabad_id = matchStore[shabadChoice]['shabad_id']
+        for num, shabad in matchStore.items():
+            if shabad['verse'] in shabadOrg['verse']:
+                shabad_id = matchStore[num]['shabad_id']
+                displayShabad(shabad_id)
+                break
+        else:
+            print("No matching shabad found")
 
 def displayShabad(shabad_id):
     chosenShabad = banidb.shabad(shabad_id)
     for ver in chosenShabad['verses']:
         print(ver['verse'])
-    
-shabadInit = input("Find Shabad: ")
-getShabadMatches(shabadInit)
+    banidb.clear()
+
+def search_from_speech():
+    rec = sr.Recognizer()
+
+    while True:
+        try:
+            with sr.Microphone() as source:
+                rec.adjust_for_ambient_noise(source, duration=0.7)
+                print("Listening...")
+                audio = rec.listen(source)
+                audioText = rec.recognize_google(audio, language='pa-in')
+
+                return audioText
+        except Exception:
+            pass
+
+def turn_into_bani_query(text):
+    bani_qeury = ''
+    for word in text.split():
+        bani_qeury +=  word[0]
+    return bani_qeury
+
+# shabadInit = input("Find Shabad: ")
+# print("Listening Shabad...")
+# shabadInput = search_from_speech()
+# print(shabadInput)
+# shabadQuery = turn_into_bani_query(shabadInput)
+# print(shabadQuery)
+# getShabadMatches(shabadQuery)
+
+# query = getShabadQuery()['query']
+getShabadMatches()
