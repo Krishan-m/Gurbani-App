a9c6f9fde43941b0bea08a8e001c1a5045ffe226
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2024-03-05 13:36:57 -0500

    Add comments and improve naming convention

--- README.txt
+++ README.txt
@@ -1,14 +1,14 @@
 This is the development space for Gurbani App using banidb library in python. Text me on whatsapp to contribute.
 
 Pre-requisites
-- Make sure you install the banidb module for the program to run
-- Install command: pip install banidb
+- Make sure you install the following modules for the program to run
+- Install command (banidb): pip install banidb
+- Install command (speech_recognition): pip install SpeechRecognition
 
 Usage:
 - Clone the repository using the https or ssh key
 - Run the main.py file
 
-----------------------------------------------------
 Operating the program:
 - Once you have the main.py file opened, run it
 - It will prompt you to enter shabad
@@ -16,16 +16,16 @@ Operating the program:
 - If shabad found, it will show you all the possible matches
 - Select the shabad number that you want.
 - Finally, it will print the selected shabad.
+
 ----------------------------------------------------
 
-**New Operating Procedures with voice search:
+New Operating Procedures with voice search:
 - Once you have the main.py file opened, run it
 - It will show that the program is listening
 - Recite the shabad that you want (complete shabad, not the initial letters of every word)
-- Approx one second after you are done, it will show the list of possible matches
+- Once you are done, it will show the list of possible matches
 - Select the shabad number that you want.
 - Finally, it will print the selected shabad
 
-------------TODO-----------------
-- Currently, the getShabadMatches() function is not taking any parameter which it was taking in last update
-- It should be modified in such a way that it still takes argument. Maybe by using reccursions
+TODO:
+1. Make the algorithm to return matching probability for the verse of shabad and the user input
--- main.py
+++ main.py
@@ -1,57 +1,91 @@
 import banidb
 import speech_recognition as sr
 
-def getShabadQuery():
-    # return input("Find Shabad: ")
-    speechInput = search_from_speech()
-    return {'query': turn_into_bani_query(speechInput), 
-            'verse': speechInput}
-
-def getShabadMatches():
+def get_shabad_query():
+    """
+    Gets the user input by listening to the audio and then invokes get_shabad_matches() once the user input is taken and validated
+    """
     while True:
-        try:
-            shabadOrg = getShabadQuery()
-            shabadQuery = shabadOrg['query']
-            shabadVerse = shabadOrg['verse']
-            matchingShabads = banidb.search(shabadQuery, searchtype=0)
-            break
-        except Exception as e:
-            print(e)
+        speech_input = search_from_speech()
+        shabad_query = turn_into_bani_query(speech_input)
+        if len(shabad_query) > 2:
+            get_shabad_matches(shabad_query, speech_input)
+            break                
 
-    matchStore = {}
-    print(shabadOrg)
-    for matches in matchingShabads['pages_data'].values():
+def get_shabad_matches(shabad_query, shabad_verse=""):
+    """
+    Invoked by get_shabad_query
+
+    Prints matching shabads and asks user to select their shabad
+    """
+    # TODO: Display the best match of shabad based on user input
+
+    print("Finding matching shabads...")
+    matching_shabad_collection = {}
+    probability_threshold = 0
+
+    matching_shabads = banidb.search(shabad_query)
+    for matches in matching_shabads['pages_data'].values():
         for shabad in matches:
-            print(f"{len(matchStore) + 1}. {shabad['verse']} ({shabad['source']['writer']})")
-            matchStore[len(matchStore) + 1] = shabad
+            probability_threshold = get_match_probability(shabad_verse, shabad['verse'])
+            if probability_threshold > 80: # 80 is just a rough guess for match probability for now
+                print(f"{len(matching_shabad_collection) + 1}. {shabad['verse']} ({shabad['source']['writer']})")
+                matching_shabad_collection[len(matching_shabad_collection) + 1] = shabad
 
-    if (len(matchStore) == 0): print("No results found") 
+    if (len(matching_shabad_collection) == 0): print("No results found") 
     else: 
-        print(f"{len(matchStore)} results found.")
-        shabadChoice = eval(input("Enter shabad number: "))
-        shabad_id = matchStore[shabadChoice]['shabad_id']
-        displayShabad(shabad_id)
-        # for num, shabad in matchStore.items():
-        #     if shabad['verse'] in shabadOrg['verse']:
-        #         shabad_id = matchStore[num]['shabad_id']
-        #         displayShabad(shabad_id)
-        #         break
-        # else:
-        #     print("No matching shabad found")
+        print(f"{len(matching_shabad_collection)} results found.")
+        shabad_choice = eval(input("Enter shabad number: "))
+        try:
+            shabad_id = matching_shabad_collection[shabad_choice]['shabad_id']
+        except KeyError:
+            print("Not a valid option!")
+        else:
+            display_shabad(shabad_id)
 
-def displayShabad(shabad_id):
-    chosenShabad = banidb.shabad(shabad_id)
-    for ver in chosenShabad['verses']:
+def get_match_probability(speech_input, shabad_verse):
+    """
+    Gets the matching probability between the user input and the shabad verse
+
+    Returns 100 for exact match of shabad (should return 100 but is not returning more than 30)
+    """
+    matching_words = 0
+    speech_input = speech_input.split()
+    shabad_verse = shabad_verse.split()
+
+    # TODO: Decide factors to reduce number of comparisons
+    # TODO: Calculate probability such that it returns 100 for exact match
+    for i in range(len(shabad_verse)):
+        try:
+            if speech_input[i] in shabad_verse:
+                matching_words += 1
+        except IndexError:
+            pass
+    print((matching_words/len(shabad_verse)) * 100)
+    return (matching_words/len(shabad_verse)) * 100
+
+def display_shabad(shabad_id):
+    """
+    Searches banidb by the shabad_id and print the shabad part of the result after extracting it from json
+    """
+    chosen_shabad = banidb.shabad(shabad_id)
+    for ver in chosen_shabad['verses']:
         print(ver['verse'])
-    banidb.clear()
+    banidb.clear() # Not quite sure what this does. Maybe clears the cache. But it is still making a cache.dat in my folder which contains a json file for shabad on reading
 
 def search_from_speech():
-    rec = sr.Recognizer()
+    """
+    Gets the voice input from user
 
-    while True:
+    Returns the final user input
+    """
+    # Initializing a Recognizer object
+    rec = sr.Recognizer()
+    while True: # Listens until it could interpret the input
         try:
+            # Making a Microphone object
             with sr.Microphone() as source:
-                rec.adjust_for_ambient_noise(source, duration=0.7)
+                rec.adjust_for_ambient_noise(source, duration=0.5)
                 print("Listening...")
                 audio = rec.listen(source)
                 audioText = rec.recognize_google(audio, language='pa-in')
@@ -61,19 +95,14 @@ def search_from_speech():
             pass
 
 def turn_into_bani_query(text):
+    """
+    Turns the user input into a bani query. i.e. Taking first letter of each word and joining together
+
+    Returns the bani query
+    """
     bani_qeury = ''
     for word in text.split():
         bani_qeury +=  word[0]
     return bani_qeury
 
-# shabadInit = input("Find Shabad: ")
-# print("Listening Shabad...")
-# shabadInput = search_from_speech()
-# print(shabadInput)
-# shabadQuery = turn_into_bani_query(shabadInput)
-# print(shabadQuery)
-# getShabadMatches(shabadQuery)
-
-# query = getShabadQuery()['query']
-
-getShabadMatches()
+get_shabad_query()
