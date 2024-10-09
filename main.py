ab997e58b13bf0bf4b99fc52e205c99d5f94c542
Krishan-m <124686301+Krishan-m@users.noreply.github.com>
2024-10-09 14:26:51 -0400

    Improve logic to filter bani query

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-import banidb
-import speech_recognition as sr
+# Database for gurbani resources
+import banidb 
 
-def get_shabad_query():
+# Help recognize speech
+import speech_recognition as sr 
+
+# Convert Gurmukhi into Unicode and vice versa
+from gurmukhiutils.constants import VOWEL_LETTERS, BASE_LETTERS
+
+# ---- Ignore this part of the code ----
+# Keep in mind - "L", "S", "Z" for later referrence
+# MATRA_UNICODE = ["i", "o", "u", "w", "y", "H", "I", "M", "N", "O", "R", "U", "W", "Y", "[", "]", "`", "~", "@", "ü", "®", "\u00b4", "\u00a8", "µ", "æ", "Í", "Î", "Ï", "Ò", "Ú", "\u02c6", "\u02dc", "ਂ\u200dੀ"]
+# --------------------------------------
+
+def get_shabad_query() -> None:
     """
     Gets the user input by listening to the audio and then invokes get_shabad_matches() once the user input is taken and validated
     """
@@ -12,43 +23,50 @@ def get_shabad_query():
             get_shabad_matches(shabad_query, speech_input)
             break                
 
-def get_shabad_matches(shabad_query, shabad_verse=""):
+def get_shabad_matches(shabad_query, shabad_verse="") -> None:
     """
-    Invoked by get_shabad_query
-
-    Prints matching shabads and asks user to select their shabad
+    Invoked by get_shabad_query.
+    
+    Prints matching shabads and asks user to select their shabad.
     """
     # TODO: Display the best match of shabad based on user input
-
     print(f"Finding matching shabads for \"{shabad_verse}\"")
+    print(f"Bani Query: {shabad_query}")
+    
     matching_shabad_collection = {}
     probability_threshold = 0
+    max_matched_shabad_probability = 0
+    max_matched_shabad_number = ""
+    max_matched_shabad_id = 0
 
     matching_shabads = banidb.search(shabad_query)
-    print("-----------Best Shabad Matches-----------")
-    for matches in matching_shabads['pages_data'].values():
-        for shabad in matches:
-            probability_threshold = get_match_probability(shabad_verse, shabad['verse'])
-            if probability_threshold > 66: # 66 is based on conclusion that if two words are matched out of 3 based on the smallest required query length
-                print(f"{len(matching_shabad_collection) + 1}. {shabad['verse']} ({shabad['source']['writer']})")
-                matching_shabad_collection[len(matching_shabad_collection) + 1] = shabad
-
-    if (len(matching_shabad_collection) == 0): print("No results found") 
+    if (matching_shabads['total_results'] == 0): print("No results found") 
     else: 
-        print(f"{len(matching_shabad_collection)} results found.")
-        while True:
-            try:
-                shabad_choice = eval(input("Enter shabad number: "))
-                shabad_id = matching_shabad_collection[shabad_choice]['shabad_id']
-            except KeyError:
-                print("Not a valid option!")
-            except SyntaxError:
-                print("Enter something!")
-            except NameError:
-                print("Try Again!")
-            else:
-                display_shabad(shabad_id)
-                break
+        print("-----------Best Shabad Matches-----------")
+        for matches in matching_shabads['pages_data'].values():
+            for shabad in matches:
+                probability_threshold = get_match_probability(shabad_verse, shabad['verse'])
+                if (probability_threshold > 66) and (probability_threshold > max_matched_shabad_probability): # 66 is based on conclusion that if two words are matched out of 3 based on the smallest required query length
+                    # print(f"{len(matching_shabad_collection) + 1}. {shabad['verse']} ({shabad['source']['writer']})")
+                    # matching_shabad_collection[len(matching_shabad_collection) + 1] = shabad
+                    max_matched_shabad_id = shabad['shabad_id']
+                    max_matched_shabad_probability = probability_threshold
+        display_shabad(max_matched_shabad_id)
+
+        # print(f"{len(matching_shabad_collection)} results found.")
+        # while True:
+        #     try:
+        #         shabad_choice = eval(input("Enter shabad number: "))
+        #         shabad_id = matching_shabad_collection[shabad_choice]['shabad_id']
+        #     except KeyError:
+        #         print("Not a valid option!")
+        #     except SyntaxError:
+        #         print("Enter something!")
+        #     except NameError:
+        #         print("Try Again!")
+        #     else:
+        #         display_shabad(shabad_id)
+        #         break
 
 def get_match_probability(speech_input, shabad_verse):
     """
@@ -56,7 +74,6 @@ def get_match_probability(speech_input, shabad_verse):
 
     Returns 100 for exact match of shabad (should return 100 but is not returning more than 30)
     """
-    matching_words = 0
     matched_words = []
     speech_input = speech_input.split()
     shabad_verse = shabad_verse.split()
@@ -65,10 +82,10 @@ def get_match_probability(speech_input, shabad_verse):
     # TODO: Calculate probability such that it returns 100 for exact match
     for i in speech_input:
         if i in shabad_verse:
-            matching_words += 1
             matched_words.append(i)
-            
-    return (matching_words/len(speech_input)) * 100
+
+    print(matched_words)    
+    return (len(matched_words)/len(speech_input)) * 100
 
 def display_shabad(shabad_id):
     """
@@ -77,7 +94,7 @@ def display_shabad(shabad_id):
     chosen_shabad = banidb.shabad(shabad_id)
     for ver in chosen_shabad['verses']:
         print(ver['verse'])
-    banidb.clear() # Not quite sure what this does. Maybe clears the cache. But it is still making a cache.dat in my folder which contains a json file for shabad on reading
+    banidb.clear() # Not quite sure what this does. Maybe clears the cache. But it is still making a cache.dat in the folder which contains a json file for shabad on reading
 
 def search_from_speech():
     """
@@ -93,22 +110,35 @@ def search_from_speech():
             with sr.Microphone() as source:
                 rec.adjust_for_ambient_noise(source, duration=0.5)
                 print("Listening...")
-                audio = rec.listen(source)
-                audioText = rec.recognize_google(audio, language='pa-in')
-
+                audio = rec.listen(source, 5) # To limit the recording duration, the parameter phrase_time_limit could be given
+                audioText = rec.recognize_google(audio, language='pa-in') # set language to 'punjabi'
                 return audioText
         except Exception:
             pass
 
 def turn_into_bani_query(text):
     """
+    NOTE: It is only removing matravas and correcting vowels
+    TODO: Remove all kinds of special characters
     Turns the user input into a bani query. i.e. Taking first letter of each word and joining together
 
     Returns the bani query
     """
     bani_qeury = ''
     for word in text.split():
-        bani_qeury +=  word[0]
+        word_initial = word[0]
+        if word_initial in VOWEL_LETTERS: # Turn vowels into simple letters
+            match word_initial:
+                case "ਅ" | "ਆ" | "ਐ" | "ਔ":
+                    word_initial = "ਅ"
+                case "ਏ" | "ਇ" | "ਈ":
+                    word_initial = "ੲ"
+                case "ਓ" | "ਉ" | "ਊ":
+                    word_initial = "ੳ"
+        elif word_initial not in BASE_LETTERS:
+            word_initial = ""
+        bani_qeury +=  word_initial # Filtered word
     return bani_qeury
 
 get_shabad_query()
+banidb.clear()
